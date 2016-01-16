1a7dc0cbd009ad44929a26d1a09593dbe0be9411
Lich <longzy2013@gmail.com>
2016-01-16 15:57:12 +0800

    Update header.blade.php

--- app/views/admin/header.blade.php
+++ app/views/admin/header.blade.php
@@ -7,8 +7,7 @@
 
     <!-- 可选的Bootstrap主题文件（一般不用引入） -->
     <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-
-    {{HTML::style("bootstrap-combined.min.css")}}
+    <link rel="stylesheet" href="//redrock.cqupt.edu.cn/weixin_activity/bootstrap-combined.min.css">
 
     <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
     <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
